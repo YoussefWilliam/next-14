fd112b0e2bab50ee4f8698185334ba1c2bd7c72b
YoussefWilliam <youssef.william997@gmail.com>
2024-02-10 00:40:04 +0200

    init commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsSubPage = ({ params }) => {
+  return (
+    <div>
+      MealsSubPage
+      <p>{params.slug}</p>
+    </div>
+  );
+};
+
+export default MealsSubPage;
