0b9479835197b82ed7d45ff90d1cbeb9907be306
YoussefWilliam <youssef.william997@gmail.com>
2024-02-10 17:57:59 +0200

    create db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,28 @@
-import React from "react";
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals-grid/meals-grid";
 
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/meals/share">Share your favorite meal recipes</Link>
+          </div>
+        </div>
+      </header>
+      <main classes={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
